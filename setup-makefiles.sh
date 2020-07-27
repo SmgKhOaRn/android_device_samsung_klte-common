e742692c006c14e61e9bf5febd10eec9754449e8
SmgKhOaRn <smgkhoarn@gmail.com>
2020-07-27 15:38:20 +0900

    klte-common: Fix setup-makefiles.sh
    Signed-off-by: SmgKhOaRn <smgkhoarn@gmail.com>

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 #
-<<<<<<< HEAD
-# Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017 The LineageOS Project
-=======
-# Copyright (C) 2017-2018 The LineageOS Project
->>>>>>> msm8974-common/lineage-17.1
+# Copyright (C) 2014 The CyanogenMod Project
+# Copyright (C) 2020 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -22,25 +18,13 @@
 
 set -e
 
-<<<<<<< HEAD
 export INITIAL_COPYRIGHT_YEAR=2014
-=======
->>>>>>> msm8974-common/lineage-17.1
 
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-<<<<<<< HEAD
 CM_ROOT="$MY_DIR"/../../..
-=======
-# Override anything that may come from the calling environment
-CM_ROOT="$MY_DIR"/../../..
-BOARD=msm8974
-DEVICE_COMMON=${BOARD}-common
-INITIAL_COPYRIGHT_YEAR=2017
-VENDOR=samsung
->>>>>>> msm8974-common/lineage-17.1
 
 HELPER="$CM_ROOT"/vendor/lineage/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
@@ -52,18 +36,19 @@ fi
 # Initialize the helper for common device
 setup_vendor "$DEVICE_COMMON" "$VENDOR" "$CM_ROOT" true
 
-<<<<<<< HEAD
 # Copyright headers and common guards
 write_headers "klte klteactivexx kltechn kltechnduo klteduos kltedv kltekdi kltekor kltespr kltesprsports klteusc kltevzw"
-=======
-# Copyright headers
-write_headers $BOARD TARGET_BOARD_PLATFORM
->>>>>>> msm8974-common/lineage-17.1
 
 write_makefiles "$MY_DIR"/common-proprietary-files.txt
 
+# Blobs for TWRP data decryption
+cat << EOF >> "$BOARDMK"
+ifeq (\$(WITH_TWRP),true)
+TARGET_RECOVERY_DEVICE_DIRS += vendor/$VENDOR/$DEVICE_COMMON/proprietary
+endif
+EOF
+
 write_footers
-<<<<<<< HEAD
 
 if [ ! -z $VARIANT_COPYRIGHT_YEAR ]; then
     export INITIAL_COPYRIGHT_YEAR=$VARIANT_COPYRIGHT_YEAR
@@ -80,7 +65,3 @@ for BLOB_LIST in "$MY_DIR"/../$DEVICE/device-proprietary-files*.txt; do
 done
 
 write_footers
-
-./../msm8974-common/setup-makefiles.sh $@
-=======
->>>>>>> msm8974-common/lineage-17.1
